8d117a5d2f4186f7f5dce3d93d4a8f9d13b28fbb
Morpheus4005 <reynardadrian2004@gmail.com>
2025-06-01 22:59:56 +0700

    Update energyBarAdjuster.cs
    balls

--- Refactored/Health, Shield etc/energyBarAdjuster.cs
+++ Refactored/Health, Shield etc/energyBarAdjuster.cs
@@ -1,31 +1,50 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnergyBarAdjuster : MonoBehaviour
-{
-    [SerializeField] private Slider energyBar;
-    public void SetEnergy(EnergyValue currentEnergy)
-    {
-        if (energyBar != null)
-        {
-            energyBar.value = currentEnergy.Value;
-        }
-    }
-    public void SetMaxEnergy(EnergyValue maxEnergy)
-    {
-        if (energyBar != null)
-        {
-            energyBar.maxValue = maxEnergy.Value;
-            energyBar.value = maxEnergy.Value;
-        }
-    }
-}
 public struct EnergyValue
 {
     public float Value { get; }
 
     public EnergyValue(float value)
     {
-        Value = Mathf.Max(0f, value);
+        Value = Mathf.Max(0f, value); 
     }
 }
+
+public class EnergyBarUI : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    public void SetEnergy(EnergyValue energy)
+    {
+        if (slider != null)
+        {
+            slider.value = energy.Value;
+        }
+    }
+
+    public void SetMaxEnergy(EnergyValue energy)
+    {
+        if (slider != null)
+        {
+            slider.maxValue = energy.Value;
+            slider.value = energy.Value;
+        }
+    }
+}
+
+public class EnergyBarAdjuster : MonoBehaviour
+{
+    [SerializeField] private EnergyBarUI energyBarUI;
+
+    public void SetEnergy(EnergyValue energy)
+    {
+        energyBarUI.SetEnergy(energy);
+    }
+
+    public void SetMaxEnergy(EnergyValue energy)
+    {
+        energyBarUI.SetMaxEnergy(energy);
+    }
+}
+
