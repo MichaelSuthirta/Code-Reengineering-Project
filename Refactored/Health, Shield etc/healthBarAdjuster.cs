f80ac744982d4f6351fb8c1cad3634b4cf813534
Gilmore <gilmoree937@gmail.com>
2025-06-03 16:35:19 +0700

    Refactor ShieldBar and HealthBar

--- Refactored/Health, Shield etc/healthBarAdjuster.cs
+++ Refactored/Health, Shield etc/healthBarAdjuster.cs
@@ -5,17 +5,13 @@ using UnityEngine.UI;
 
 public class healthBarAdjuster : MonoBehaviour
 {
-    //Storing health bar slider
     public Slider healthBarSlider;
-
-    //Setting max HP
     public void setMaxHP(float hp)
     {
         healthBarSlider.maxValue = hp;
         healthBarSlider.value = hp;
     }
 
-    // Changing HP
     public void setHP(float hp)
     {
         healthBarSlider.value = hp;
--- Refactored/Health, Shield etc/shieldBarAdjuster.cs
+++ Refactored/Health, Shield etc/shieldBarAdjuster.cs
@@ -5,19 +5,13 @@ using UnityEngine.UI;
 
 public class shieldBarAdjuster : MonoBehaviour
 {
-    //Script of player stats
     public playerStats playerStats;
-
-    //Sprites
     public Sprite shieldNone;
     public Sprite shieldFill;
-
-    //Array to store the objects
     public Image[] shieldBar;
 
     public void fillBar(int currentShield)
     {
-        //Filling the bar according to current shield amount
         for (int i = 0; i < shieldBar.Length; i++)
         {
             if (i < currentShield)
@@ -30,11 +24,8 @@ public class shieldBarAdjuster : MonoBehaviour
             }
         }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //Making sure it doesn't display shield more than max
         for(int i = 0; i < shieldBar.Length; i++)
         {
             if(i < playerStats.playerMaxShield)
